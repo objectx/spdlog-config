789ac37c610b9ef402f74d29f28e762d96fd2632
Masashi Fujita <objectxtreme@gmail.com>
2015-10-27 00:04:24 +0900

    feat(config): Share sink definitions across the channels

--- src/spdlog-config.cpp
+++ src/spdlog-config.cpp
@@ -13,43 +13,94 @@
 using namespace std ;
 using namespace spdlog_config::detail ;
 
-void configure_logger (std::shared_ptr<cpptoml::table> toml) {
-    auto channels = toml->get_table_array ("channels");
-    if (!channels) {
-        return;
+using toml_t = std::shared_ptr<cpptoml::table> ;
+
+static std::tuple<std::string, std::vector<spdlog::sink_ptr>> read_sinks (toml_t toml) {
+    auto name = toml->get_as<std::string> ("name") ;
+    if (! name) {
+        throw std::runtime_error { "No sink names" } ;
     }
+    auto outputs = toml->get_table_array ("output") ;
+    if (! outputs) {
+        throw std::runtime_error { "No outputs" };
+    }
+    std::vector<spdlog::sink_ptr>   result ;
+    for (auto o : *outputs) {
+        result.emplace_back (create_sink (o)) ;
+    }
+    return std::make_tuple (*name, result) ;
+}
 
-    std::set<std::string> channel_names;
+void configure_logger (std::shared_ptr<cpptoml::table> toml) {
+    // Default pattern
+    auto default_pattern = toml->get_as<std::string> ("pattern").value_or ("%+") ;
+    auto default_level = toml->get_as<std::string> ("level").value_or ("off") ;
+    auto default_sink = toml->get_as<std::string> ("default_sink") ;
 
-    for (auto chan : *channels) {
-        std::vector<spdlog::sink_ptr> sinks;
-
-        for (auto sink : *chan->get_table_array ("sinks")) {
-            sinks.push_back (create_sink (sink));
+    auto get_level = [&default_level](auto lv) {
+        if (! lv) {
+            return parse_log_level (default_level) ;
         }
-        if (sinks.empty ()) {
-            sinks.push_back (create_null_sink (nullptr));
+        else {
+            return parse_log_level (*lv) ;
         }
-        auto const name = chan->get_as<std::string> ("name");
-        if (!name) {
-            throw std::runtime_error { "Missing channel names." };
+    } ;
+    auto get_sink_name = [&default_sink](auto name) {
+        if (! name) {
+            if (! default_sink) {
+                throw std::runtime_error { "No sink names" };
+            }
+            return *default_sink ;
         }
-        if (channel_names.find (*name) != end (channel_names)) {
-            throw std::runtime_error {
-                    std::string { "Channel name \"" }.append (*name).append ("\" was appeard twice.") };
+        else {
+            return *name ;
         }
-        auto const level_name = chan->get_as<std::string> ("level");
-        auto       level      = parse_log_level (level_name.value_or ("off"));
+    } ;
+    auto get_pattern = [&default_pattern] (auto pat) {
+        if (! pat) {
+            return default_pattern ;
+        }
+        else {
+            return *pat ;
+        }
+    } ;
 
-        auto logger = std::make_shared<spdlog::logger> (*name, begin (sinks), end (sinks));
-        logger->set_level (level);
-
-        auto const pattern = chan->get_as<std::string> ("pattern");
-        if (pattern) {
-            logger->set_pattern (*pattern);
+    std::map<std::string, std::vector<spdlog::sink_ptr>>    sink_dict ;
+    {
+        auto sinks = toml->get_table_array ("sink") ;
+        if (! sinks) {
+            throw std::runtime_error { "No sinks" } ;
         }
+        for (auto s : *sinks) {
+            std::string name ;
+            std::vector<spdlog::sink_ptr>   outputs ;
+            std::tie (name, outputs) = read_sinks (s) ;
+            sink_dict [name] = outputs ;
+        }
+    }
+    {
+        auto channels = toml->get_table_array ("channel") ;
+        if (! channels) {
+            throw std::runtime_error { "No channels" };
+        }
+        for (auto ch : *channels) {
+            auto name = ch->get_as<std::string> ("name") ;
+            if (! name) {
+                throw std::runtime_error { "No channel names" };
+            }
+            auto lvl       = get_level (ch->get_as<std::string> ("level")) ;
+            auto sink_name = get_sink_name (ch->get_as<std::string> ("sink")) ;
+            auto pat = get_pattern (ch->get_as<std::string> ("pattern")) ;
 
-        spdlog::register_logger (logger);
+            auto it = sink_dict.find (sink_name) ;
+            if (it == sink_dict.cend ()) {
+                throw std::runtime_error { std::string { "Bad sink name \"" }.append (sink_name).append ("\" found") };
+            }
+            auto logger = std::make_shared<spdlog::logger> (*name, begin (it->second), end (it->second)) ;
+            logger->set_level (lvl) ;
+            logger->set_pattern (pat) ;
+            spdlog::register_logger (logger) ;
+        }
     }
 }
 
--- test/main.cpp
+++ test/main.cpp
@@ -1,2 +1,67 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+
+#include <spdlog-config.hpp>
+
+static const char * toml_text [] =
+    { "# Logger configuration"
+    , "pattern      = \"%+\""
+    , "level        = \"info\""
+    , "default_sink = \"default\""
+    , ""
+    , "[[sink]]"
+    , "    name       = \"default\""
+    , "    [[sink.output]]"
+    , "        type       = \"null\""
+    , "    [[sink.output]]"
+    , "        type       = \"stdout\""
+    , "    [[sink.output]]"
+    , "        type       = \"stderr\""
+    , "[[sink]]"
+    , "    name       = \"file\""
+    , "    [[sink.output]]"
+    , "        type       = \"file\""
+    , "        path       = \"file.log\""
+    , "    [[sink.output]]"
+    , "        type       = \"rotating-file\""
+    , "        path       = \"rotate\""
+    , "        extension  = \"log\""
+    , "        max_size   = \"1024M\""
+    , "        keep       = 3"
+    , "    [[sink.output]]"
+    , "        type       = \"daily-file\""
+    , "        path       = \"daily\""
+    , "        extension  = \"log\""
+    , "        refresh_on = \"12:00\""
+    , ""
+    , "[[channel]]"
+    , "    name  = \"global\""
+    , "    level = \"info\""
+    , "    sink  = \"default\""
+    , ""
+    , "[[channel]]"
+    , "    name  = \"var\""
+    , "    level = \"debug\""
+    , "    sink  = \"file\""
+    , nullptr } ;
+
+static void test_logger () {
+    std::stringstream  S ;
+    for (auto p = toml_text ; *p != nullptr ; ++p) {
+        S << *p << std::endl ;
+    }
+    S.seekg (0) ;
+    configure_logger (S) ;
+    spdlog::get ("global")->info ("Logged") ;
+    spdlog::get ("var")->debug ("debug") ;
+}
+
+int main (int argc, char * const argv[]) {
+    // global setup...
+
+    int result = Catch::Session ().run (argc, argv);
+
+    // global clean-up...
+    //test_logger () ;
+    return result;
+}
