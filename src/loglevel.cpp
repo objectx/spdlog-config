11bbaa17bc1ad5b702034f9ac904b8a0dfc30709
objectx <objectxtreme@gmail.com>
2015-10-27 21:44:51 +0900

    fix(config): Fix failed CLion's refactorings

--- src/loglevel.cpp
+++ src/loglevel.cpp
@@ -10,9 +10,9 @@
 #include "internal.hpp"
 
 namespace spdlog_config { namespace detail {
-    spdlog::level::level_enum parse_log_level (const std::__1::string & s) {
+    spdlog::level::level_enum parse_log_level (const std::string & s) {
         struct level_desc_t {
-            std::__1::string          name_;
+            std::string          name_;
             spdlog::level::level_enum level_;
 
             level_desc_t (const char * name, int lvl)
@@ -20,7 +20,7 @@ namespace spdlog_config { namespace detail {
                 /* NO-OP */
             }
         };
-        const static std::__1::array<level_desc_t, 10>
+        const static std::array<level_desc_t, 10>
                 levels { level_desc_t { "trace", 0 }
                          , level_desc_t { "debug", 1 }
                          , level_desc_t { "info", 2 }
@@ -36,20 +36,20 @@ namespace spdlog_config { namespace detail {
 
         for (size_t i = 0; i < levels.size (); ++i) {
             auto const & lv = levels[i].name_;
-            if (strncasecmp (lv.c_str (), s.c_str (), std::__1::min (s.size (), lv.size ())) == 0) {
+            if (strncasecmp (lv.c_str (), s.c_str (), std::min (s.size (), lv.size ())) == 0) {
                 if (lv.size () == s.size ()) {
                     // Found exact match
                     return levels[i].level_;
                 }
                 if (found != nullptr) {
                     throw std::runtime_error {
-                            std::__1::string { "Log level \"" }.append (s).append ("\" is ambiguous.") };
+                            std::string { "Log level \"" }.append (s).append ("\" is ambiguous.") };
                 }
                 found = &levels[i];
             }
         }
         if (found == nullptr) {
-            throw std::runtime_error { std::__1::string { "Unknown log level \"" }.append (s).append ("\" found.") };
+            throw std::runtime_error { std::string { "Unknown log level \"" }.append (s).append ("\" found.") };
         }
         return found->level_;
     }
